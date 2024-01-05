7275c576b70bd4bb9d9a14400adffa2c12309fc3
ManiKumar830 <mohanmanikumar8309@gmail.com>
2024-01-05 16:37:53 +0530

    Assignment

---
+++ src/components/About/index.js
@@ -0,0 +1,28 @@
+import {Link} from "react-router-dom"
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+   <div className='about-items-container'>
+
+  
+     <div className='text-container'>
+        <h1 className='role'>UI/UX DESIGNER</h1>
+        <h1 className='main-title'>Hello,  my name is Madelyn Torff</h1>
+        <p className='description'>Short text with details about you, what you do or your professional career. You can add more information on the about page.</p>
+        <div>
+         <Link to={"/projects"}>
+          <button className='projects-button'>Projects</button>
+         </Link>
+         
+          <button className='linkedIn-buttom'>LinkedIn</button>
+        </div>
+     </div>
+      <img alt='logo' className='image' src="https://hmp.me/ebw0"/>
+      </div>
+
+     
+  </div>
+)
+
+export default About
